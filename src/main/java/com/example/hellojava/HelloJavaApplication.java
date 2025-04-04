e3a9ee6fba18c661ddef09620696963b99903400
My Name <shubhamwaadhankar123@gmail.com>
2025-04-04 18:01:12 +0530

    first

---
+++ src/main/java/com/example/hellojava/HelloJavaApplication.java
@@ -0,0 +1,11 @@
+package com.example.hellojava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloJavaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloJavaApplication.class, args);
+    }
+}
